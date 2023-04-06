e7c3fca5307cd86520cbf732a62bd6c45e076416
Karthikeyan Nallathambi <karthik.aspx.cs@gmail.com>
2023-04-06 20:36:10 +0000

    Components Stub Created

--- pages/index.js
+++ pages/index.js
@@ -1,40 +1,36 @@
 import {createClient} from 'contentful'
 import DiningCard from '../components/DinerCard';
+import Logo from '../components/Logo';
+import HeroCarousel from '../components/HeroCarousel';
+import Navigation from '../components/Navigation';
+import PageDetail from '../components/PageDetail';
 
-export async function getStaticProps(){
-  const client = createClient({
-    space: process.env.CONTENTFUL_SPACE_ID,
-    accessToken: process.env.CONTENTFUL_ACCESS_TOKEN
-  });
+// export async function getStaticProps(){
+//   const client = createClient({
+//     space: process.env.CONTENTFUL_SPACE_ID,
+//     accessToken: process.env.CONTENTFUL_ACCESS_TOKEN
+//   });
 
-  const res = await client.getEntries({content_type: 'dining'});
+//   const res = await client.getEntries({content_type: 'dining'});
 
-  return{
-    props: {
-      diners: res.items,
-      revalidate: 1
-    }
-  }
-}
+//   return{
+//     props: {
+//       diners: res.items,
+//       revalidate: 1
+//     }
+//   }
+// }
 
 export default function Recipes({diners}) {
   console.log(diners)
   return (
-    <div className="diner-list">
-      {diners.map(diner => (
-        <DiningCard key={diner.sys.key} diner={diner} />
-      ))}
-
-      <style jsx>{`
-          .diner-list{
-            display:grid;
-            grid-template-columns: auto auto auto;
-            grid-gap:10px;
-          }
-        `}
-      </style>
-
+    <div>
+      <Logo></Logo>
+      <Navigation>
+        
+      </Navigation>
+      <HeroCarousel></HeroCarousel>
+      <PageDetail></PageDetail>
     </div>
-    
   )
 }
