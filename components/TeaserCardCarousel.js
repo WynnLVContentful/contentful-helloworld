42765eb705b9a9887b7e1e9c593ec8f92b38ec0c
Yonathan ghilom <95502254+yonathanghilom@users.noreply.github.com>
2023-04-12 00:21:22 +0000

    remove scroll when the mobile nav bar is open

--- components/TeaserCardCarousel.js
+++ components/TeaserCardCarousel.js
@@ -6,11 +6,12 @@ export default function TeaserCardCarousel() {
     return (
         <section className="teaser-card">
             <div className="container">
+                <h2 className="mb-5">Information Address</h2>
                 <div className="teaser-card-carousel">
                     <div className="row row-cols-1 row-cols-md-2 row-cols-lg-3 teaser-card-holder">
                         <div className="col mb-3">
-                            <div className="card  border-0  shadow-lg ">
-                                <Image src={firstCard} width={414} height={332} className=" card-img-top carou-image  shadow-lg" alt="first content descriptions" />
+                            <div className="card  border-0    ">
+                                <Image src={firstCard} width={414} height={332} className=" card-img-top carou-image   " alt="first content descriptions" />
                                 <div className="card-body">
                                     <div className="card-title">
                                         <h3 className="text-left mb-4">OUR PEOPLE</h3>
@@ -32,8 +33,8 @@ export default function TeaserCardCarousel() {
                             </div>
                         </div>
                         <div className="col mb-3 ">
-                            <div className="card  border-0  shadow-lg ">
-                                <Image src={firstCard} width={414} height={332} className=" shadow-lg card-img-top carou-image " alt="first content descriptions" />
+                            <div className="card  border-0    ">
+                                <Image src={firstCard} width={414} height={332} className="   card-img-top carou-image " alt="first content descriptions" />
                                 <div className="card-body">
                                     <div className="card-title">
                                         <h3 className="text-left mb-4">OUR PEOPLE</h3>
@@ -55,8 +56,8 @@ export default function TeaserCardCarousel() {
                             </div>
                         </div>
                         <div className="col mb-3 ">
-                            <div className="card  border-0  shadow-lg ">
-                                <Image src={firstCard} width={414} height={332} className="shadow-lg  card-img-top carou-image " alt="first content descriptions" />
+                            <div className="card  border-0    ">
+                                <Image src={firstCard} width={414} height={332} className="   card-img-top carou-image " alt="first content descriptions" />
                                 <div className="card-body">
                                     <div className="card-title">
                                         <h3 className="text-left mb-4">OUR PEOPLE</h3>
@@ -78,8 +79,8 @@ export default function TeaserCardCarousel() {
                             </div>
                         </div>
                         <div className="col mb-3 ">
-                            <div className="card  border-0  shadow-lg ">
-                                <Image src={firstCard} width={414} height={332} className="shadow-lg  card-img-top carou-image " alt="first content descriptions" />
+                            <div className="card  border-0    ">
+                                <Image src={firstCard} width={414} height={332} className="   card-img-top carou-image " alt="first content descriptions" />
                                 <div className="card-body">
                                     <div className="card-title">
                                         <h3 className="text-left mb-4">OUR PEOPLE</h3>
@@ -95,75 +96,6 @@ export default function TeaserCardCarousel() {
                                 </div>
                                 <div className="card-footer bg-white border-0">
                                     <Link href="detail-id-page/test1" className="link-footer-card btn-link-item">
-                                        <span className="btn-link">Read More</span>
-                                    </Link>
-                                </div>
-                            </div>
-                        </div>
-                        <div className="col mb-3">
-                            <div className="card  border-0  shadow-lg ">
-                                <Image src={firstCard} width={414} height={332} className="shadow-lg  card-img-top carou-image " alt="first content descriptions" />
-                                <div className="card-body">
-                                    <div className="card-title">
-                                        <h3 className="text-left mb-4">OUR PEOPLE</h3>
-                                    </div>
-                                    <div className="content">
-                                        <ul className="text-left content_list">
-                                            <li><span class="card-text content_item">Human Capital Management</span></li>
-                                            <li><span class="card-text content_item">Employee Benefits</span></li>
-                                            <li><span class="card-text content_item">Diversity and Inclusion</span></li>
-                                            <li><span class="card-text content_item">Training and Developmen</span></li>
-                                        </ul>
-                                    </div>
-                                </div>
-                                <div className="card-footer bg-white border-0">
-                                    <Link href="detail-id-page/test1" className="link-footer-card btn-link-item">
-                                        <span className="btn-link">Read More</span>
-                                    </Link>
-                                </div>
-                            </div>
-                        </div>
-                        <div className="col mb-3 ">
-                            <div className="card  border-0  shadow-lg ">
-                                <Image src={firstCard} width={414} height={332} className="shadow-lg  card-img-top carou-image " alt="first content descriptions" />
-                                <div className="card-body">
-                                    <div className="card-title">
-                                        <h3 className="text-left mb-4">OUR PEOPLE</h3>
-                                    </div>
-                                    <div className="content">
-                                        <ul className="text-left content_list">
-                                            <li><span class="card-text content_item">Human Capital Management Human Capital Management Human Capital Management Human Capital Management Human Capital Management</span></li>
-                                            <li><span class="card-text content_item">Employee Benefits</span></li>
-                                            <li><span class="card-text content_item">Diversity and Inclusion</span></li>
-                                            <li><span class="card-text content_item">Training and Developmen</span></li>
-                                        </ul>
-                                    </div>
-                                </div>
-                                <div className="card-footer bg-white border-0">
-                                    <Link href="detail-id-page/test1" className="link-footer-card btn-link-item">
-                                        <span className="btn-link">Read More</span>
-                                    </Link>
-                                </div>
-                            </div>
-                        </div>
-                        <div className="col mb-3 ">
-                            <div className="card border-0 shadow-lg">
-                                <Image src={firstCard} width={414} height={332} className="shadow-lg  card-img-top carou-image " alt="first content descriptions" />
-                                <div className="card-body">
-                                    <div className="card-title">
-                                        <h3 className="text-left mb-4">OUR PEOPLE</h3>
-                                    </div>
-                                    <div className="content">
-                                        <ul className="text-left content_list">
-                                            <li><span class="card-text content_item">Human Capital Management Human Capital Management Human Capital Management Human Capital Management Human Capital Management</span></li>
-                                            <li><span class="card-text content_item">Employee Benefits</span></li>
-                                            <li><span class="card-text content_item">Diversity and Inclusion</span></li>
-                                            <li><span class="card-text content_item">Training and Developmen</span></li>
-                                        </ul>
-                                    </div>
-                                </div>
-                                <div className="card-footer bg-white border-0">
-                                    <Link href="detail-id-page/test1" className="link-footer-card btn-link">
                                         <span className="btn-link">Read More</span>
                                     </Link>
                                 </div>
