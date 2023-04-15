5043a880f0b8415c89da52a2ad01e689a5790b8e
Yonathan Ghilom <yonathan.ghilom@wynnlasvegas.com>
2023-04-14 17:19:17 -0700

    connect column files

--- components/PageDetail.js
+++ components/PageDetail.js
@@ -1,21 +1,51 @@
 import { Fragment } from "react";
-import { documentToReactComponents } from '@contentful/rich-text-react-renderer';
+import { documentToReactComponents } from "@contentful/rich-text-react-renderer";
 
-export default function PageDetail({compProps}) {
-    if(!compProps) return;
-    const {title, text} = compProps.fields;
-    const texts = (text.content[0].content[0].value);
+export default function PageDetail({ compProps }) {
+  if (!compProps) return;
+  console.log(compProps)
+  const { title, text } = compProps.fields;
+  const texts = text.content[0].content[0].value;
+  const textsContents = text.content;
+  let fileType = {};
+  textsContents.map((item) => {
+    fileType[item.nodeType] = item.content;
+  });
+//   console.log(fileType);
+  function DetailsThreecolumncontent() {
     return (
-        
-        <section className="mt-5 ">
-            <div className="container">
-                <div className="detail-page text-center">
-                    <h1>{title}</h1>
-                    <p> 
-                       documentToReactComponents{texts}
-                    </p>
-                </div>        
-            </div>
-        </section>
+      <Fragment>
+        <div className="card-title ">
+          <h3 class="card-title text-center">Net-Zero by 2050</h3>
+        </div>
+        <div class="card-body">
+          <li>
+            <span class="card-text">
+              To reduce or offset all carbon dioxide (CO2) produced by our
+              operations no later than 2050.
+            </span>
+          </li>
+        </div>
+      </Fragment>
     );
+  }
+  function PageDetailcontent() {
+    return (
+      <Fragment>
+        <section className="mt-5 ">
+          <div className="container">
+            <div className="detail-page text-center">
+              <h1>{title}</h1>
+              <p>documentToReactComponents{texts}</p>
+            </div>
+          </div>
+        </section>
+      </Fragment>
+    );
+  }
+  if (compProps.fields.layout == "Page Detail") {
+    return <PageDetailcontent />;
+  } else if (compProps.fields.layout == "Content") {
+    return <DetailsThreecolumncontent />;
+  }
 }
--- components/ThreeColumnLayout.js
+++ components/ThreeColumnLayout.js
@@ -1,24 +1,39 @@
 import Image from "next/image";
+import PageDetail from "./PageDetail";
 
-function ThreeColumnLayout({compProps}) {
-console.log(compProps);
-
-    return (
-        <section className="two-column reverse-position-image mt-5">
-            <div className="container">
-                <div className="reverse-position-image">
-                    <div className="two-column-image-left">
-                       
-                    </div>
-                    <div className="two-column-text-right">
-                        
-                    </div>
-                    <div className="two-column-image-left">
-                       
-                    </div>
-                </div>
+function ThreeColumnLayout({ compProps }) {
+    if(!compProps) return;
+  const { column1, column2, column3 } = compProps.fields;
+//   console.log(column1);
+  return (
+    <section className={`three-column pb-3 mt-5`}>
+      <div className="container">
+        <div className="row row-cols-1 row-cols-lg-3 g-3 three-column-list ">
+          <div className="col">
+            <div class="card border border-gray shadow-sm mb-1 rounded">
+              <div className="two-column-image-left">
+                <PageDetail compProps={column1} />
+              </div>
             </div>
-        </section>);
+          </div>
+          <div className="col">
+            <div class="card border border-gray shadow-sm mb-1 rounded">
+              <div className="two-column-image-left">
+                <PageDetail compProps={column2} />
+              </div>
+            </div>
+          </div>
+          <div className="col">
+            <div class="card border border-gray shadow-sm mb-1 rounded">
+              <div className="two-column-image-left">
+                <PageDetail compProps={column3} />
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+  );
 }
 
 export default ThreeColumnLayout;
