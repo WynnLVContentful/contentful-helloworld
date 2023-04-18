f9ad7e35aaa9d68fcc46e241f416097e0b0e2aa3
Yonathan Ghilom <yonathan.ghilom@wynnlasvegas.com>
2023-04-18 16:20:51 -0700

    foooter fixed

--- components/Footer.js
+++ components/Footer.js
@@ -1,68 +1,70 @@
 import Link from "next/link";
 import { Fragment, useEffect, useState } from "react";
-import {createClient} from 'contentful'
-import { documentToReactComponents } from '@contentful/rich-text-react-renderer';
-
-
+import { createClient } from "contentful";
+import { documentToReactComponents } from "@contentful/rich-text-react-renderer";
 
 export default function Footer() {
+  const [columns, setColumns] = useState([]);
+  async function fetchData() {
+    try {
+      const client = createClient({
+        space: "vsfvp3vjns8g",
+        accessToken: "QY1yhc6cZbV4FNiVoOrEPvKjbEyHjcgs5-5mZeQTqik",
+      });
 
-    const [columns, setColumns] = useState([]);
+      const { items } = await client.getEntries({
+        content_type: "footerNavigation",
+        "fields.title": "Footer",
+        include: 10,
+      });
 
-    useEffect(() => { 
-        async function fetchData() {
-            try {
-                const client = createClient({
-                    space: "vsfvp3vjns8g",
-                    accessToken: "QY1yhc6cZbV4FNiVoOrEPvKjbEyHjcgs5-5mZeQTqik"
-                  });
-            
-                const {items} = await client.getEntries({content_type:'footerNavigation', 'fields.title' : 'Footer', include: 10});
+      console.log(["Footer", items[0]]);
 
-                console.log(['Footer', items[0]]);
+      setColumns(items[0].fields.columns);
+    } catch (err) {
+      console.log(err);
+    }
+  }
+  useEffect(() => {
+    fetchData();
+  }, []);
 
-                setColumns(items[0].fields.columns);
-
-            } catch (err) {
-                console.log(err);
+  return (
+    <footer className="footer">
+      <div className="container">
+        <div className="footer-content row row-cols-1 row-cols-lg-3">
+          {columns.map((column) => {
+            switch (column.sys.contentType.sys.id) {
+              case "navigation":
+                return (
+                  <ul className="col list-unstyled ">
+                    {column.fields.menus.map((menu) => {
+                      return (
+                        <li>
+                          <Link href={menu.fields.url}>
+                            {menu.fields.label}
+                          </Link>
+                        </li>
+                      );
+                    })}
+                  </ul>
+                );
+              case "textBlock":
+                return (
+                  <div className="col list-unstyled ">
+                    <h3>{column.fields.title}</h3>
+                  </div>
+                );
             }
-        }
-        fetchData();
-    }, []);
+          })}
+        </div>
 
-    return (
-        <footer className="footer">
-            <div className="container">
-                {columns.map(column => {
-                    switch(column.sys.contentType.sys.id)
-                    {
-                        case "navigation":
-                            return(
-                                <ul>
-                                    {column.fields.menus.map(menu =>{
-                                        return (
-                                            <li>
-                                                <Link href={menu.fields.url}>
-                                                    {menu.fields.label}
-                                                </Link>
-                                            </li>
-                                        )
-                                    })}
-                                </ul>
-                            )                  
-                        case "textBlock":
-                            return (
-                                <div>
-                                    <h3>{column.fields.title}</h3>
-                                    
-                                </div>
-                                )
-                           
-                    }
-                })}
-               
-                <p className="copy-right"><span>&#169; </span>{new Date().getFullYear()} Wynn Resorts Holdings, LLC. All Rights Reserved.</p>
-            </div>
-        </footer>
-    );
+        <p className="copy-right">
+          <span>&#169; </span>
+          {new Date().getFullYear()} Wynn Resorts Holdings, LLC. All Rights
+          Reserved.
+        </p>
+      </div>
+    </footer>
+  );
 }
