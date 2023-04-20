cf88bd28da160042b884ef00b4a701a7eafb90dc
Yonathan Ghilom <yonathan.ghilom@wynnlasvegas.com>
2023-04-19 17:09:29 -0700

    update ver -2 preview

--- pages/[slug].js
+++ pages/[slug].js
@@ -1,49 +1,61 @@
-import {createClient} from 'contentful'
-import React, { Fragment } from 'react';
-import RenderComponents from '../components/RenderComponents';
+import { createClient } from "contentful";
+import React, { Fragment } from "react";
+import RenderComponents from "../components/RenderComponents";
 
-import InvesterCanvas from '../components/invester-files-canvas';
+import InvesterCanvas from "../components/invester-files-canvas";
+import PreviewExit from "../components/preview-exit";
 
-export async function getStaticPaths(){
+export async function getStaticPaths() {
   const client = createClient({
     space: "vsfvp3vjns8g",
-    accessToken: "QY1yhc6cZbV4FNiVoOrEPvKjbEyHjcgs5-5mZeQTqik"
+    accessToken: "QY1yhc6cZbV4FNiVoOrEPvKjbEyHjcgs5-5mZeQTqik",
   });
 
-  const res = await client.getEntries({content_type:'page'});
-  
-  const paths = res.items.map(item => {
-        return  { params: { slug : item.fields.slug } } 
+  const res = await client.getEntries({ content_type: "page" });
+
+  const paths = res.items.map((item) => {
+    return { params: { slug: item.fields.slug } };
   });
 
   return {
     paths,
-    fallback: false
-  }
+    fallback: false,
+  };
 }
 
-export async function getStaticProps({params}){
-    const client = createClient({
-      space: "vsfvp3vjns8g",
-      accessToken: "QY1yhc6cZbV4FNiVoOrEPvKjbEyHjcgs5-5mZeQTqik"
-    });
-  
-    const {items} = await client.getEntries({include: 10, content_type: 'page', 'fields.slug' : params.slug });
-  
-    return{
-      props: {
-        page: items[0],
-        revalidate: 1
-      }
-    }
-  }
+export async function getStaticProps(context) {
+  const contentfulClients = createClient({
+    space: "vsfvp3vjns8g",
+    accessToken: "QY1yhc6cZbV4FNiVoOrEPvKjbEyHjcgs5-5mZeQTqik",
+  });
+  const previewsContent = createClient({
+    space: "vsfvp3vjns8g",
+    environment: "master", // defaults to 'master' if not set
+    accessToken: "RgsQUYjSQI1_FA6Sf2IMlC1yPaWqlhOhUvWhAaq8g1c",
+    host: "preview.contentful.com",
+  });
+  const client = context.preview ? previewsContent : contentfulClients;
+  const { items } = await client.getEntries({
+    include: 10,
+    content_type: "page",
+    "fields.slug": context.params.slug,
+  });
+  return {
+    props: {
+      preview : context.preview || false,
+      page: items[0],
+      revalidate: 1,
+    },
+  };
+}
 
-export default function LandingPage({page}) {
-  const {content} = page.fields;
+export default function LandingPage({ page,preview }) {
+  const { content } = page.fields;
 
   return (
     <Fragment>
+      {preview && <PreviewExit/>}
       <RenderComponents components={content}></RenderComponents>
     </Fragment>
-  )
+  );
 }
